de384a037f3545f8593300137e1ea4cca5bad023
Hermxn <hermanhrynko@gmail.com>
2024-05-21 17:30:02 +0200

    CardNote styles refactored

--- notespace/frontend/src/cards/CardNoteNew.js
+++ notespace/frontend/src/cards/CardNoteNew.js
@@ -2,7 +2,7 @@ export default function CardNoteNew({}) {
   return (
     <div className="CardNote CardNoteCreate">
       <a className="CardNoteHref CardNoteNewHref" href={"notes/create"}>
-        <div className="CardContent CardContentCreate">Create note</div>
+        <div className="CardContent CardContentCreate">Add note</div>
       </a>
     </div>
   );
